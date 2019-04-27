89f7813099911ea35bdbb11bac53fb0978be3e7a
Nisan Ercan Tarhan <nisan_tarhan@hotmail.com>
2019-04-27 14:54:41 +0300

    TodoList without router

--- App.js
+++ App.js
@@ -8,8 +8,8 @@
 
 import React, { Component } from 'react';
 import { Platform, AsyncStorage, StyleSheet, Text, View, TextInput, Dimensions, FlatList, Alert } from 'react-native';
-import { MyButton, Item, Header, Title, Description, TodoList, AddTodo } from './src/components/main';
-import { Router, Scene } from 'react-native-router-flux';
+import { MyButton, Item, Header, Title, Description } from './src/components/main';
+// import { Router, Scene } from 'react-native-router-flux';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 
 const { height, width } = Dimensions.get('window');
@@ -116,7 +116,7 @@ export default class App extends Component {
 
   }
 
-  onChangeText = (text) => this.setState({ text })}
+  // onChangeText = (text) => this.setState({ text })}
   // deleteUserId = async () => {
   //   try {
   //     await AsyncStorage.removeItem('userId');
@@ -133,25 +133,31 @@ export default class App extends Component {
   render() {
     let { title, description } = this.state;
     return (
-      <Router style={styles.container}>
-        <Stack key="root">
-          <Scene key="home" title="TodoList" component={TodoList} />
-
-          <Scene key="add" title="AddTodo" component={AddTodo} />
-
-          {/* <Header /> */}
-          {/* <View style={{ borderBottomColor: '#e74c3c', borderBottomWidth: 1 }}></View> */}
-        </Stack>
-      </Router>
+      <View style={styles.container}>
+          <Header />
+           <Title value={title} onChangeText={(title) => this.setState({ title })} maxLength={34} placeholder='Başlık giriniz' />
+        <Description value={description} onChangeText={(description) => this.setState({ description })} placeholder='Açıklama giriniz' maxLength={76} multiline={true} numberOfLines={5} />
+        <View style={{ height: height * 0.17, backgroundColor: '#34495e', alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }}>
+            <MyButton onClick={this.addItem} text={'ADD'}></MyButton>
+            <MyButton onClick={this.deleteAllItem} text={'DELETE ALL'}></MyButton>
+        </View>
+          <View style={{ borderBottomColor: '#e74c3c', borderBottomWidth: 1 }}></View>
+    
+          <View style={{ flex: 1, backgroundColor: '#34495e' }}>
+            <FlatList
+                data={this.state.items}
+                keyExtractor={(item, index) => item.description + index.toString()}
+                renderItem={this.renderItem}
+            />
+          </View>
+          </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
     justifyContent: 'center'
   }
-
 });
--- src/components/addTodo.js
+++ src/components/addTodo.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, Dimensions } from 'react-native';
-import { MyButton, Title, Description } from './src/components/main';
-import { Actions } from 'react-native-router-flux';
-const { height, width } = Dimensions.get('window');
+// import React, { Component } from 'react';
+// import { Text, View, TouchableOpacity, Dimensions } from 'react-native';
+// import { MyButton, Title, Description } from './src/components/main';
+// import { Actions } from 'react-native-router-flux';
+// const { height, width } = Dimensions.get('window');
 
-const AddTodo = (props) => {
-    return (
-        <Title value={title} onChangeText={(title) => this.setState({ title })} maxLength={34} placeholder='Başlık giriniz' />
-        <Description value={description} onChangeText={(description) => this.setState({ description })} placeholder='Açıklama giriniz' maxLength={76} multiline={true} numberOfLines={5} />
-        <View style={{ height: height * 0.17, backgroundColor: '#34495e', alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }}>
-            <MyButton onClick={this.addItem} text={'ADD'}></MyButton>
-            <MyButton onClick={this.deleteAllItem} text={'DELETE ALL'}></MyButton>
-        </View>
-    );
-}
+// const AddTodo = (props) => {
+//     return (
+    
+//     );
+// }
 
-export default AddTodo;
+// export default AddTodo;
